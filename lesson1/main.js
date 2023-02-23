99a04ead94c2b66f43eef0626c7da8f850cb6f6d
nazariikravets <goodtovar1@gmail.com>
2023-02-23 18:52:17 +0200

    Initial commit

---
+++ lesson1/main.js
@@ -0,0 +1,45 @@
+let hello = "hello";
+let owu = "owu";
+let com = "com";
+let ua = "ua";
+let number1 = 1;
+let number_999 = -999;
+let number123 = 123;
+let numberP = 3.14;
+let number2_7 = 2.7;
+let number16 = 16;
+let truen = true;
+let fals = false;
+console.log(hello);
+console.log(owu);
+console.log(com);
+console.log(ua);
+console.log(number1);
+console.log(number_999);
+console.log(number123);
+console.log(numberP);
+console.log(number2_7);
+console.log(number16);
+console.log(truen);
+console.log(fals);
+
+
+let firstName = "Назарій";
+let lastName = "Кравець";
+let middleName = "Юрійович";
+
+let person = `${firstName} ${lastName} ${middleName}`;
+console.log(person);
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
+let name = prompt("name");
+let lname = prompt("lastname");
+let mname = prompt("middlename");
+console.log(`${name} ${lname} ${mname}`)
