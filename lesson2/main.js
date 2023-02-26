29857df0c2d25c979f9232eb9c575c55e79f7a29
nazariikravets <goodtovar1@gmail.com>
2023-02-26 20:53:21 +0200

    finish lesson 2

---
+++ lesson2/main.js
@@ -0,0 +1,63 @@
+let array = [12, 43, "bom", "don", false, 432, 43534, 6, 4, 7];
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+console.log(array[4]);
+console.log(array[5]);
+console.log(array[6]);
+console.log(array[7]);
+console.log(array[8]);
+console.log(array[9]);
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+
+let book1 = {title: "tom", pageCount: 234, genre: "history"};
+let book2 = {title: "bom", pageCount: 134, genre: "criminal"};
+let book3 = {title: "jey", pageCount: 254, genre: "western"};
+console.log(book1);
+console.log(book2);
+console.log(book3);
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
+
+
+
+
+
+
+
+let bookAuthors1 = {title: "tom", pageCount: 234, genre: "history",authors:["stefan",32]};
+let bookAuthors2 = {title: "bom", pageCount: 134, genre: "criminal",authors:["oleg",42]};
+let bookAuthors3 = {title: "som", pageCount: 334, genre: "western",authors:["ivan",52]};
+console.log(bookAuthors1);
+console.log(bookAuthors2);
+console.log(bookAuthors3);
+// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors. Поле "автори" - масив. Кожен автор має поля name,age
+
+
+
+let array12=[
+    {name:"ivan",username:"obmb",password:11},
+    {name:"petro",username:"ghgbmb",password:12},
+    {name:"ivan",username:"obmb",password:13},
+    {name:"ivan",username:"obmb",password:14},
+    {name:"ivan",username:"obmb",password:15},
+    {name:"ivan",username:"obmb",password:16},
+    {name:"ivan",username:"obmb",password:17},
+    {name:"ivan",username:"obmb",password:18},
+    {name:"ivan",username:"obmb",password:19},
+    {name:"ivan",username:"obmb",password:20}
+]
+console.log(array12[0].password);
+console.log(array12[1].password);
+console.log(array12[2].password);
+console.log(array12[3].password);
+console.log(array12[4].password);
+console.log(array12[5].password);
+console.log(array12[6].password);
+console.log(array12[7].password);
+console.log(array12[8].password);
+console.log(array12[9].password);
+
+
+// - Створити масив з 10 об'єктами які описують сутніть "користувач". Поля: name, username,password. Вивести в консоль пароль кожного користувача
